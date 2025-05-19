24e50ea21ca8b4d31be83fe9bfaa552a41c61580
vmdt <vmdt03@gmail.com>
2025-05-19 22:12:06 +0700

    [setup]: init notification-worker

---
+++ pkg/mongodb/mongo_options.go
@@ -0,0 +1,9 @@
+package mongodb
+
+type MongoDbOptions struct {
+	Host     string `mapstructure:"host"`
+	Port     int    `mapstructure:"port"`
+	User     string `mapstructure:"user"`
+	Password string `mapstructure:"password"`
+	Database string `mapstructure:"database"`
+}
---
+++ pkg/mongodb/mongodb.go
@@ -0,0 +1,41 @@
+package mongodb
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+const (
+	connectTimeout  = 60 * time.Second
+	maxConnIdleTime = 3 * time.Second
+	minPoolSize     = 20
+	maxPoolSize     = 300
+)
+
+func NewMongoDB(cfg *MongoDbOptions) (*mongo.Client, error) {
+	uriAddress := fmt.Sprintf(
+		"mongodb://%s:%s@%s:%d",
+		cfg.User,
+		cfg.Password,
+		cfg.Host,
+		cfg.Port,
+	)
+
+	opt := options.Client().ApplyURI(uriAddress).
+		SetConnectTimeout(connectTimeout).
+		SetMaxConnIdleTime(maxConnIdleTime).
+		SetMinPoolSize(minPoolSize).
+		SetMaxPoolSize(maxPoolSize)
+
+	ctx := context.Background()
+	client, err := mongo.Connect(ctx, opt)
+	if err != nil {
+		return nil, fmt.Errorf("failed to connect to MongoDB: %w", err)
+	}
+
+	return client, nil
+}
